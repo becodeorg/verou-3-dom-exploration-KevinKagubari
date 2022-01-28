0ad1a1d5751f9b4e9d9b82e34134a1c797e8c8c2
Brocolit <k.kagubari89@gmail.com>
2022-01-28 10:55:42 +0100

    js file

---
+++ script.js
@@ -0,0 +1,10 @@
+
+console.log(document.title);
+
+// This does not change the previous statement
+
+document.title = "Modifying the DOM";
+
+const body = document.body
+
+body.style.background = "#FF69B4";
