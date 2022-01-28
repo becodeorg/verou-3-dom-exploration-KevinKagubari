401fe8a2d224dafcd7837912e8a429f793f1f016
Brocolit <k.kagubari89@gmail.com>
2022-01-28 16:54:16 +0100

    Einde oefing

--- script.js
+++ script.js
@@ -8,3 +8,32 @@ document.title = "Modifying the DOM";
 const body = document.body
 
 body.style.background = "#FF69B4";
+
+console.log(document.body.children);
+
+
+
+const kids = document.body.children;
+
+
+/*for_of loop uses the indexes of the array as your let that way you don't have to 'count'
+*/
+
+for (let kid of kids) {
+    
+    console.log(kid)
+    
+    
+}
+
+
+/*
+
+This does the same as the loop above.
+
+for (let i=0; i < kids.length; i++){
+
+    console.log(kids[i])
+}
+*/
+
